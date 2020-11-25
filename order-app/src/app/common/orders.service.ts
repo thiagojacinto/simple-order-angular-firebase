369e04570513ee23f0055f915c59c14eb5ceae07
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-11-24 21:33:14 -0300

    feat: implements FormGroup & FormControl into service

--- order-app/src/app/common/orders.service.ts
+++ order-app/src/app/common/orders.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,11 @@ import { Injectable } from '@angular/core';
 export class OrdersService {
 
   constructor() { }
+
+  form = new FormGroup({
+    customerName: new FormControl(""),
+    orderNumber: new FormControl(""),
+    productOrder: new FormControl(""),
+    completed: new FormControl("")
+  });
 }
