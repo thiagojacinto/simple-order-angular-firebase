9589da2e4549af3549bb2a365cee993a42e6078b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-11-24 21:32:25 -0300

    feat: importing ReactiveForms module

--- order-app/src/app/app.module.ts
+++ order-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { OrdersService } from "./common/orders.service";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     OrdersService
