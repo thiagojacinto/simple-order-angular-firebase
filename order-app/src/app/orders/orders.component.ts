d1bcb4ccdbf80f083d1f93adbe016d267a00f389
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-11-24 21:34:04 -0300

    feat: fixed list of coffees & inject of service

--- order-app/src/app/orders/orders.component.ts
+++ order-app/src/app/orders/orders.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../common/orders.service';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
+  coffees = [
+    'Americano',
+    'Flat White',
+    'Cappuccino',
+    'Latte',
+    'Espresso',
+    'Machiato',
+    'Mocha',
+    'Hot Chocolate',
+    'Tea',
+  ];
 
-  constructor() { }
+  constructor(public orderService: OrdersService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
