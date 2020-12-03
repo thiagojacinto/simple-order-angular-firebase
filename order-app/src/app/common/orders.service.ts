f2b5eda28c269d6a202bc00e963d96132687214b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-12-02 23:11:42 -0300

    feat: service methods Update & Delete

--- order-app/src/app/common/orders.service.ts
+++ order-app/src/app/common/orders.service.ts
@@ -3,34 +3,48 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
 
-  constructor(private firestore: AngularFirestore) { }
+  coffeeOrdersList = [];
+
+  constructor(private firestore: AngularFirestore) {}
 
   form = new FormGroup({
-    customerName: new FormControl(""),
-    orderNumber: new FormControl(""),
-    productOrder: new FormControl(""),
-    completed: new FormControl("")
+    customerName: new FormControl(''),
+    orderNumber: new FormControl(''),
+    productOrder: new FormControl(''),
+    completed: new FormControl(''),
   });
 
   createCoffeeOrder(orderData) {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
       this.firestore
-        .collection("coffeOrders")
+        .collection('coffeOrders')
         .add(orderData)
         .then(
           (res) => resolve(),
           (err) => reject(err)
-          );
+        );
     });
   }
 
   getCoffeeOrders() {
+    return this.firestore.collection('coffeOrders').snapshotChanges();
+  }
+
+  updateCoffeeOrder(orderData) {
     return this.firestore
-      .collection("coffeOrders")
-      .snapshotChanges();
+      .collection('coffeOrders')
+      .doc(orderData.payload.doc.id)
+      .set({ completed: true }, { merge: true });
+  }
+
+  removeCoffeeOrder = (orderData) => {
+    return this.firestore
+      .collection('coffeOrders')
+      .doc(orderData.payload.doc.id)
+      .delete();
   }
 }
